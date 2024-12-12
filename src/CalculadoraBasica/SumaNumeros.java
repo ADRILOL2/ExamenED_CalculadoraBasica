42c26509f2c75662206db162d795fa8b0caaed73
ADRILOL2 <aavila20479@iesjoanramis.org>
2024-12-12 18:57:54 +0100

    Cambio realizado desde feature-suma

--- src/CalculadoraBasica/SumaNumeros.java
+++ src/CalculadoraBasica/SumaNumeros.java
@@ -2,6 +2,7 @@ package CalculadoraBasica;
 import java.util.Scanner;
 public class SumaNumeros {
 
+	//Cambio realizado desde feature-suma
 	public static void main(String[] args) {
 		//Añade dos numeros en distintas variables para luego mostrar estos mismos y ademas de la suma de ambos
 		Scanner scanner = new Scanner(System.in);
@@ -11,7 +12,6 @@ public class SumaNumeros {
 		int numero2 = scanner.nextInt();
 		int suma = numero1 + numero2;
 		System.out.println("La suma de " + numero1 + " y " + numero2 + " es: " + suma);
-
 	}
 
 }
